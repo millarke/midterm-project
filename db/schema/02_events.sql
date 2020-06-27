c052fd3f6572a4cc27440e1b6d5b8f0103e27714
Keith Millar <keithmillar@hotmail.ca>
2020-06-27 16:25:42 +0000

    created events table from ERD

---
+++ db/schema/02_events.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS events CASCADE;
+
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  location VARCHAR(255),
+  uniqueURL VARCHAR(255)
+)
