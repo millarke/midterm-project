bf319c563f934cec3942bf80c3914276bb516aff
Keith Millar <keithmillar@hotmail.ca>
2020-06-27 17:53:54 +0000

    changed users seed file

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@email.com');
+INSERT INTO users (name, email) VALUES ('Kira', 'hotsauce88@hotmail.com');
+INSERT INTO users (name, email) VALUES ('Steven', 'buddyboy@gmail.com');
