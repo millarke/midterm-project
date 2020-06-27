ed663509daa58f27f9ac60950379cbf85c492a70
Keith Millar <keithmillar@hotmail.ca>
2020-06-27 16:48:39 +0000

    added dates table, added comment to events file to remind us to resolve the ON DELETE CASCADE question we have

--- db/schema/02_events.sql
+++ db/schema/02_events.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS events CASCADE;
 
 CREATE TABLE events (
   id SERIAL PRIMARY KEY NOT NULL,
+  -- do we need or want on delete cascade here? double check how this works. we want events being deleted if users delete their accounts, but the problem is that we don't even have user accounts.
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
