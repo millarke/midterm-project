bf20459e04c8961e37c678e0073099de15332dfa
julimancan <julian.m.bustos@gmail.com>
2020-06-28 17:19:21 -0400

    added redirect form / to home and helpers.js file with a random string generator for the event url

--- server.js
+++ server.js
@@ -2,19 +2,22 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+const sass = require("node-sass-middleware");
+const app = express();
+const morgan = require('morgan');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
+
+const { generateRandomString } = require("./helpers");
+// const newUrl = generateRandomString();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -46,9 +49,16 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
+
+
+app.get("/home", (req, res) => {
   res.render("index");
 });
+
+app.get("/", (req, res) => {
+  res.redirect("/home")
+})
+
 
 app.get("/new", (req, res) => {
   res.render("new_event");
