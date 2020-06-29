c4a5ccb993e98fa048f4d8e828736be8e6a83148
julimancan <julian.m.bustos@gmail.com>
2020-06-29 18:12:04 -0400

    prevented dates page from reloading upon submitting new date options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,6 +12,16 @@ $(() => {
 });
 
 
+$(document).ready(function() {
+     // do your things
+    var form = $('#add-new-date-to-options');
+    form.submit(function(e) {
+        // prevent form submission
+        e.preventDefault();
+    return false;
+   });
+});
+
 
 
 
