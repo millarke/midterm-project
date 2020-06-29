a8482223425587af286a37094faa4270685315e6
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-29 18:13:29 -0400

    fixed nav bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,15 @@
 
 
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
-});
-
-
-
-
-
 // $(() => {
 //   $.ajax({
 //     method: "GET",
-//     url: "/new"
-//   }).done((events) => {
-//     // console.log("I have returned with", events)
-//     // for(user of users) {
-//     //   $("<div>").text(user.name).appendTo($("body"));
-
-//   })
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });
 // });
+
 
 
